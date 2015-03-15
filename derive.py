2aa85639567e2ed04486978929231e878344457f
Jonah Witcig <jonah.witcig@cerner.com>
2015-03-15 11:51:08 -0500

    Correcting and improving.

--- derive.py
+++ derive.py
@@ -1,15 +1,40 @@
 from general import delta_x, delta_y, slope
+from models import Point
 
-point1 = {'x': 1, 'y': 1}
-point2 = {'x': 3, 'y': 7}
+# point1 = Point(2, 3)
+# point2 = Point(5, 8)
+#
+# delta_x = delta_x(point1, point2)
+# x = point1.x
+# y = point1.y
+#
+# max_slope = slope(point1, point2) * 2
+#
+# x_increment = 0.0000000001
+# increment_count = delta_x / x_increment
+#
+# slope_increment = max_slope / increment_count
+#
+# curr_slope = slope_increment
+#
+# while curr_slope <= max_slope:
+#     x += x_increment
+#     y += curr_slope * x_increment
+#
+#     curr_slope += slope_increment
+#
+# print y
+
+point1 = Point(2, 3)
+point2 = Point(5, 8)
 
 delta_x = delta_x(point1, point2)
-x = point1['x']
-y = point1['y']
+x = point1.x
+y = point1.y
 
 max_slope = slope(point1, point2) * 2
 
-x_increment = 0.0001
+x_increment = 0.0000000001
 increment_count = delta_x / x_increment
 
 slope_increment = max_slope / increment_count
--- general.py
+++ general.py
@@ -1,10 +1,10 @@
 def slope(point1, point2):
-    delta_x = point2['x'] - point1['x']
-    delta_y = point2['y'] - point1['y']
-    return delta_y / delta_x
+    delta_x = point2.x - point1.x
+    delta_y = point2.y - point1.y
+    return float(delta_y) / float(delta_x)
 
 def delta_x(point1, point2):
-    return abs(point2['x'] - point1['x'])
+    return abs(point2.x - point1.x)
 
 def delta_y(point1, point2):
-    return abs(point2['y'] - point1['y'])
+    return abs(point2.y - point1.y)
