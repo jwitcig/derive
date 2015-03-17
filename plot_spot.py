73363dd267907fc4ce61dc29aaec55b967d5e093
Jonah Witcig <jonah.witcig@cerner.com>
2015-03-16 20:27:07 -0500

    Removing unnecessary calculations.

--- plot_spot.py
+++ plot_spot.py
@@ -3,23 +3,17 @@ from models import Point
 
 def calc_point(target, point1=None, point2=None):
     """
-    Generates  smooth curve of constant second derivative between two points.
+    Generates smooth curve of constant second derivative between two points.
     The point for the desired x value is returned.
     """
-    dx = 1.0
-
     delta_x = point2.x - point1.x
-    increment_count = delta_x / dx
     target_delta_x = target - point1.x
 
     max_slope = slope(point1, point2) * 2
-    dm = max_slope / increment_count
 
-    init_slope = 0
-
-    avg_slope = (init_slope + dm*(target_delta_x/dx)) / 2
+    target_slope = ((target_delta_x / delta_x) * max_slope)
+    avg_slope = target_slope / 2
 
     x = target
     y = (target_delta_x * avg_slope) + point1.y
-    result = Point(x, y)
-    return (result.x, result.y)
+    return Point(x, y)
